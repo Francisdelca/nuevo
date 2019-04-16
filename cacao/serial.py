ada2088312bed4d505487e82e30ea86ad2669a7f
delcadead <francisdelca@hotmail.com>
2019-04-15 20:30:41 -0500

    serial random

--- cacao/serial.py
+++ cacao/serial.py
@@ -1,14 +1,17 @@
-valor = 0
-valor2 = 0
-valor3 = 0
+from random import *
+temperatura = [10,15,20,25,30,35,40,45,50,55,60]
+humedad = [10,12,15,20,22,25,30,32,35,40,45,50,55,60,80]
+ph = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
 while 1:
-    valor = valor + 1
-    valor2 = valor2 + 2
-    valor3 = valor3 + 3
-    v = '' +str(int(valor))
-    v2 = '' +str(int(valor2))
-    v3 = '' +str(int(valor3))
+    shuffle(temperatura)
+    shuffle(humedad)
+    shuffle(ph)
+    t = '' + str (int (temperatura[0]))
+    h = '' + str (int (humedad[0]))
+    p = '' + str (int (ph[0]))
     file = open('sensor.txt','w')
-    file.write('{}\n{}\n{}'.format(v,v2,v3))
+    file.write('{}\n{}\n{}'.format(t, h, p))
     file.close()
-    print(valor)
+    print(temperatura[0])
+
+
